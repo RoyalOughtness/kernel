3c58c8553fd6afe5231593ba74c073170427538a
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-09-14 23:33:43 -0700

    Update copr_script.sh

--- copr_script.sh
+++ copr_script.sh
@@ -249,7 +249,7 @@ configs_to_disable=(
   
   # https://cateee.net/lkddb/web-lkddb/STRICT_DEVMEM.html
   # https://cateee.net/lkddb/web-lkddb/IO_STRICT_DEVMEM.html
-  # Fedora enables these but it depends on devmem, which we disable, so we must disable it too
+  # Fedora enables these but they depends on devmem, which we disable, so we must disable them too
   CONFIG_STRICT_DEVMEM
   CONFIG_IO_STRICT_DEVMEM
 
